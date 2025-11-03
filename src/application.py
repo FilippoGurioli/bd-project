1c773cc068d967e8a724fde9413f2c770cf00948
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-11-04 00:07:17 +0100

    feat: drop columns that break logic

--- src/application.py
+++ src/application.py
@@ -52,7 +52,10 @@ def run_non_optimized(spark, trips_path, zones_path):
     t_start = time.time()
     
     # Load data
-    df_trips = spark.read.option("mergeSchema", "true").parquet(trips_path).select(
+    df_trips = spark.read
+        .option("mergeSchema", "true")
+        .drop("congestion_surcharge", "airport_fee")
+        .parquet(trips_path).select(
         "PULocationID", "tpep_pickup_datetime", "fare_amount", "tip_amount"
     )
     rdd_trips = df_trips.rdd.map(lambda r: (
@@ -133,7 +136,10 @@ def run_optimized(spark, trips_path, zones_path):
     t_start = time.time()
     
     # Load trips
-    df_trips = spark.read.option("mergeSchema", "true").parquet(trips_path).select(
+    df_trips = spark.read
+        .option("mergeSchema", "true")
+        .drop("congestion_surcharge", "airport_fee")
+        .parquet(trips_path).select(
         "PULocationID", "tpep_pickup_datetime", "fare_amount", "tip_amount"
     )
     rdd_trips = df_trips.rdd.map(lambda r: (
