6869039554115b267488145ae8e5b311ed9ea608
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-11-07 15:47:09 +0100

    refactor: move shell scripts to a personal dir

---
+++ scripts/launch-remote.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -e 0 ]; then
+  echo "Pass cluster id as first parameter"
+  return 0
+fi
+
+aws emr ssh \
+  --cluster-id $1 \
+  --key-pair-file keys/31-10-fgurioli.pem \
+  --command "spark-submit --master yarn --deploy-mode cluster --class project.Application s3://unibo-31-10-fgurioli/jars/nyc-taxi-tip-analysis_2.12-1.0.jar remote 3"
