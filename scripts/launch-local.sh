cec18fc09285ea552a129657a0527c7926c56299
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-11-07 15:48:28 +0100

    fix: update reference in scripts

--- scripts/launch-local.sh
+++ scripts/launch-local.sh
@@ -4,7 +4,7 @@ export SPARK_HOME="/home/filippo/spark"
 export PATH="$SPARK_HOME/bin:$PATH"
 
 echo "BUILDING AND PUSHING TO AWS S3"
-source ./build-and-push-aws.sh
+source ./scripts/build-and-push-aws.sh
 
 if ! curl -s --head http://localhost:18080 | grep -q "200 OK"; then
   echo "History server not running. Starting..."
